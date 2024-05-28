b50e5e46c84c13a0aaa389946c419c0c551151fd
minisudal <p66132379@gmail.com>
2024-05-28 20:44:41 +0900

    Spring

--- src/main/java/kr/ac/hnu/itup/ItupApplication.java
+++ src/main/java/kr/ac/hnu/itup/ItupApplication.java
@@ -1,13 +1,24 @@
 package kr.ac.hnu.itup;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
+import java.util.Objects;
+
+@Slf4j
 @SpringBootApplication
+@ImportResource("classpath:root-context.xml")
 public class ItupApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ItupApplication.class, args);
+        ApplicationContext context = SpringApplication.run(ItupApplication.class, args);
+        Restaurant restaurant = context.getBean("restaurant", Restaurant.class);
+//        System.out.println("restaurant: {}" + restaurant);
+        log.info("restaurant: {}", restaurant);
     }
 
 }
